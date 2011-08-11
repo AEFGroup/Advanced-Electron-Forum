43658c052be2c279b9822a84eeccfd2c6f740bc9
Cruz Bishop <cruzjbishop@gmail.com>
2011-08-11 20:01:51 +1000

    Some documentation

--- main/classes/rss.php
+++ main/classes/rss.php
@@ -25,12 +25,27 @@ if (!defined('AEF')) {
     die('Hacking Attempt');
 }
 
+/**
+ * A basic class that provides various RSS features and helpers
+ * @author Various previous
+ * @author Cruz Bishop - Refactoring and upgrade to RSS 2.0
+ */
 class rss {
 
-    //The Heading of the RSS
+    /**
+     * Inserts the RSS header
+     * @param string $xmlVersion The XML version to use
+     * @param string $encoding The character encoding to use
+     * @param string $rssVersion The RSS version to use
+     * @param string $title The title to use
+     * @param string $link The link to use
+     * @param string $desc The description (AKA content) to use
+     * @return string A string containing the RSS header
+     */
     function start($xmlVersion = '1.0', $encoding = 'UTF-8', $rssVersion = '2.0', $title = '', $link = '', $desc = '') {
 
-        return '<?xml version="' . $xmlVersion . '" encoding="' . $encoding . '"?>
+        return '
+<?xml version="' . $xmlVersion . '" encoding="' . $encoding . '"?>
 <rss version="' . $rssVersion . '" xml:lang="en-US">
     <channel>
         <title>' . $title . '</title>
@@ -38,17 +53,26 @@ class rss {
         <description>' . $desc . '</description>';
     }
 
-    //The end
+    /**
+     * Closes the RSS feed
+     * @return string The footer
+     */
     function close() {
 
-        $str = '
+        return '
     </channel>
 </rss>';
-
-        return $str;
     }
 
-    //Add an ITEM
+    /**
+     * Creates an item part of a RSS feed
+     * @param string $title The title to use
+     * @param string $link The link to use
+     * @param string $desc The description (AKA content) to use
+     * @param string $category The category to use
+     * @param string $pubDate The published date to use
+     * @return string The item part of the feed as a string
+     */
     function item($title, $link, $desc, $category, $pubDate) {
 
         return '
