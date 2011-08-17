9184fe334e2c4dec00df25652359722535314b05
Cruz Bishop <cruzjbishop@gmail.com>
2011-08-17 10:08:02 +1000

    lang part 1

---
+++ languages/english/admin/plugins_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+/////////////////////////////////////////////////////////////////
+//===============================================================
+// conpan_lang.php(languages/english/admin)
+//===============================================================
+// AEF : Advanced Electron Forum 
+// Version : 1.0.9
+// Inspired by Pulkit and taken over by Electron
+// Extract text from admin files by oxlo (16th January 2008).
+// --------------------------------------------------------------
+// Started by: Electron, Ronak Gupta, Pulkit Gupta
+// Date:       23rd Jan 2006
+// Time:       15:00 hrs
+// Site:       http://www.anelectron.com/ (Anelectron)
+// --------------------------------------------------------------
+// Please Read the Terms of use at http://www.anelectron.com
+// --------------------------------------------------------------
+//===============================================================
+// (C)AEF Group All Rights Reserved.
+//===============================================================
+/////////////////////////////////////////////////////////////////
+
+$l['list_page_title'] = 'Admin Center - Plugins';
+
+?>
