1e8096b3450e3cf24cfc41d8d87b08b74fd0fc36
Sadaoui Abderrahim <SAFAD.Line@gmail.com>
2012-06-15 08:16:43 +0100

    the installer needs universal to be present
    so here it is, enjoy it while it lasts

---
+++ universal.php
@@ -0,0 +1,50 @@
+<?php
+
+//////////////////////////////////////////////////////////////
+//===========================================================
+// universal.php
+//===========================================================
+// AEF : Advanced Electron Forum 
+// Version : 1.0.9
+// Inspired by Pulkit and taken over by Electron
+// ----------------------------------------------------------
+// Started by: Electron, Ronak Gupta, Pulkit Gupta
+// Date:       23rd Jan 2006
+// Time:       15:00 hrs
+// Site:       http://www.anelectron.com/ (Anelectron)
+// ----------------------------------------------------------
+// Please Read the Terms of use at http://www.anelectron.com
+// ----------------------------------------------------------
+//===========================================================
+// (C)AEF Group All Rights Reserved.
+//===========================================================
+//////////////////////////////////////////////////////////////
+
+/* Database Connection */
+
+$globals['user'] = '';
+$globals['password'] = '';
+$globals['database'] = '';
+$globals['dbprefix'] = '';
+$globals['server'] = '';
+
+/* Ending - Database Connection */
+
+
+
+/* Core Settings */
+
+$globals['url'] = '';
+$globals['sn'] = '';
+$globals['board_email'] = '';
+$globals['server_url'] = __DIR__;
+$globals['mainfiles'] = __DIR__ .'/main';
+$globals['themesdir'] = __DIR__ .'/themes';
+$globals['pluginsdir'] = __DIR__ .'/plugins';
+$globals['cachedir'] = __DIR__ .'/cache';
+$globals['gzip'] = 1;
+$globals['cookie_name'] = '';
+$globals['installed'] = '0';
+
+/* Ending - Core Settings */
+?>
